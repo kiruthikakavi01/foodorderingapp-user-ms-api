2554ef3d1433f1f730a3cf7fe9484beb011da798
kiruthikakavi01 <kiruthikakavi01@gmail.com>
2022-05-05 17:14:17 +0530

    first commit

---
+++ src/main/java/com/foodorderingapp/FoodorderingappUserMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodorderingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodorderingappUserMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodorderingappUserMsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodorderingapp/FoodorderingappUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodorderingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodorderingappUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
